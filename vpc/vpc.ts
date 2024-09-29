522a300b791bc30596805792c008762550364427
zamanrabeen671 <mdzamanrabin@gmail.com>
2024-09-29 21:37:08 +0600

    project initialized and add vpc

---
+++ vpc/vpc.ts
@@ -0,0 +1,21 @@
+import * as aws from "@pulumi/aws";
+
+const custom_tags = {
+    "managed_by": "cat_cloud",
+    env: "dev"
+}
+// Allocate a new VPC with a custom CIDR block.
+const vpc = new aws.ec2.Vpc("cat_vpc", {
+    cidrBlock: "10.10.0.0/16",
+    tags: custom_tags
+});
+
+const subnet = new aws.ec2.Subnet("cat-public-subnet", {
+    vpcId: vpc.id,
+    cidrBlock: "10.10.1.0/24",
+    mapPublicIpOnLaunch: true,
+    tags: custom_tags
+
+})
+export const vpcId = vpc.id;
+export const subnetId = subnet.id
